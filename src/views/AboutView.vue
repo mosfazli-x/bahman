6bd87e644a4dfec60feb2b0329051c501ec6ebe2
mosfazli-x <mosfazli.x@gmail.com>
2023-09-16 20:38:03 +0330

    add Tailwind and DaisyUI

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,3 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h3>This is about page</h3>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>This is a test</h2>
 </template>
